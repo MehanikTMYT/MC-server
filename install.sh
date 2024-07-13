a99ecfdda945e014072a028fed1fbde06c781499
MehanikTMYT <Pitter479@gmail.com>
2024-07-13 13:18:54 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt -y install openjdk-17-jdk
+
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+
+sudo apt update
+
+sudo apt install -y playit
+
+java -jar neoforge-20.4.237-installer.jar --installServer
