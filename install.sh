436e21d2b01bf839f57707d8a577a2acbf5a2779
MehanikTMYT <Pitter479@gmail.com>
2024-07-13 10:31:40 +0000

    first launch

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-wget https://maven.neoforged.net/releases/net/neoforged/neoforge/20.4.237/neoforge-20.4.237-installer.jar
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
-
 sudo apt update
-
 sudo apt install -y playit
 
+wget https://maven.neoforged.net/releases/net/neoforged/neoforge/20.4.237/neoforge-20.4.237-installer.jar
 java -jar neoforge-20.4.237-installer.jar --installServer
-
 rm -r neoforge-20.4.237-installer.jar
+rm -r run.bat
