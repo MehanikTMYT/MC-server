eb15c05c95fa034cbc91f64433c55d91f234635e
MehanikTMYT <Pitter479@gmail.com>
2024-07-17 09:34:14 +0000

    prebuild

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
-sudo apt update
-sudo apt install -y playit
-
+apt update & apt upgrade
+apt install tmux
 wget https://maven.neoforged.net/releases/net/neoforged/neoforge/20.4.237/neoforge-20.4.237-installer.jar
+wget https://github.com/AuroraTeam/ServerWrapper/releases/download/v1.1.0/sw_linux
 java -jar neoforge-20.4.237-installer.jar --installServer
 rm -r neoforge-20.4.237-installer.jar
 rm -r run.bat
